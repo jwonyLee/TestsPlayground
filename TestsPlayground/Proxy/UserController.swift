bbeea516a604734202874ee1322e30c395eef6ce
JiWon Lee <rieul@rieul.tech>
2025-12-02 20:48:50 +0900

    UserControllerProxy 구현.

--- TestsPlayground/Proxy/UserController.swift
+++ TestsPlayground/Proxy/UserController.swift
@@ -31,3 +31,34 @@ class UserControllerImpl: UserController {
     }
 }
 
+class UserControllerProxy: UserController {
+    private let userController: UserController // 근데 왜 인터페이스로 안 받지
+    
+    init(userController: UserController) {
+        self.userController = userController
+    }
+    
+    func login(
+        telephone: String,
+        password: String
+    ) -> UserVO {
+        let startTime = DispatchTime.now()
+        let userVo = userController.login(telephone: telephone, password: password)
+        let endTime = DispatchTime.now()
+        let elapsedTime = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
+        print("elapsedTime: \(elapsedTime)") // 여기서는 그냥 프린트문으로 출력하자.
+        return userVo
+    }
+    
+    func register(
+        telephone: String,
+        password: String
+    ) -> UserVO {
+        let startTime = DispatchTime.now()
+        let userVo = userController.register(telephone: telephone, password: password)
+        let endTime = DispatchTime.now()
+        let elapsedTime = endTime.uptimeNanoseconds - startTime.uptimeNanoseconds
+        print("elapsedTime: \(elapsedTime)") // 여기서는 그냥 프린트문으로 출력하자.
+        return userVo
+    }
+}
