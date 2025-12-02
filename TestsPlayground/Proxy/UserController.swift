3321104e5af5176bc4fb5ff1dae4e6c208fe604b
JiWon Lee <rieul@rieul.tech>
2025-12-02 20:48:39 +0900

    UserController 구현체 구현.

--- TestsPlayground/Proxy/UserController.swift
+++ TestsPlayground/Proxy/UserController.swift
@@ -14,3 +14,20 @@ protocol UserController {
         password: String
     ) -> UserVO
 }
+
+class UserControllerImpl: UserController {
+    func login(
+        telephone: String,
+        password: String
+    ) -> UserVO {
+        UserVO()
+    }
+    
+    func register(
+        telephone: String,
+        password: String
+    ) -> UserVO {
+        UserVO()
+    }
+}
+
